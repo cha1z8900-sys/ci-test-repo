553fae512849ca21b195fd1d6b39f6cca5d4309e
cha1z8900-sys <cha1z8900@gmail.com>
2026-06-13 06:37:46 +0100

    initial commit with failing test

---
+++ app.py
@@ -0,0 +1,7 @@
+def add (a, b):
+    return a + b
+def devide (a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+    
---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import add, devide
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+def test_devide():
+    assert devide(6, 3) == 2
+    assert devide(5, 2) == 2.5
+def test_devide_by_zero():
+    assert devide(10, 0) == 0
